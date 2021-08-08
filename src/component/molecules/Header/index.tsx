1ea2f643d851eece55f4b00364de19f49bce9d32
jyun1 <jyuny12345@gmail.com>
2021-08-08 13:40:25 +0800

    add Header

---
+++ src/component/molecules/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.scss';
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>Pick Color on Your Image</h1>
+        </div>
+    )
+};
+
