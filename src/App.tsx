96029ffb2d7c9f53f753135f5a7a220a505e19ac
jyun1 <jyuny12345@gmail.com>
2021-08-08 22:14:37 +0800

    add ImageBase64Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import './App.css';
-import Header from 'component/molecules/Header';
-import UploadZone from 'component/molecules/UploadZone';
-import MainContent from 'component/molecules/MainContent';
-import Footer from 'component/molecules/Footer';
+import { useState } from "react";
+import "./App.css";
+import Header from "component/molecules/Header";
+import UploadZone from "component/molecules/UploadZone";
+import MainContent from "component/molecules/MainContent";
+import Footer from "component/molecules/Footer";
+import { ImageBase64Context, defaultImage } from "model/imageBase64";
 
 function App() {
+  const [image, setImage] = useState(defaultImage);
+
   return (
-    <div className="App">
-      <Header />
-      <UploadZone />
-      <MainContent />
-      <Footer />
-     </div> 
+    <ImageBase64Context.Provider value={{image, setImage}}>
+      <div className="App">
+        <Header />
+        <UploadZone />
+        <MainContent />
+        <Footer />
+      </div>
+    </ImageBase64Context.Provider>
   );
 }
 
--- src/component/molecules/UploadZone/index.tsx
+++ src/component/molecules/UploadZone/index.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useDropzone } from "react-dropzone";
 import styles from "./index.module.scss";
 
 export default function UploadZone() {
+  const onDrop = React.useCallback((acceptedFiles) => {
+    const image = acceptedFiles[0];
+    let reader = new FileReader();
+
+    reader.readAsDataURL(image);
+    reader.onload = () => {
+      const imageBase64 = reader.result;
+      console.log(imageBase64);
+    };
+  }, []);
+
+  const config = {
+    accept: 'image/jpeg, image/png, image/jpg',
+    onDrop, 
+    multiple: false
+  }
+
+  const { getRootProps, getInputProps } = useDropzone(config);
+
   return (
-    <div className={styles.uploadArea}>
+    <div className={styles.uploadArea} {...getRootProps()}>
+      <input {...getInputProps()} />
       <p className={styles.uploadCTA}>
         {`Drop Your File Here or `}
         <span>Select It</span>
